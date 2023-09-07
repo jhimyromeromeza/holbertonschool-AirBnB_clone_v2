f1fbddbec0022e29e15125a64af8e0a4a696c58d
0skarmp <omoralespj@gmailcom>
2023-09-06 22:27:27 -0700

    fix bugs in file database sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,21 +1,7 @@
-db = MySQLdb.connect(
-    host='localhost',
-    port=3306,
-    user='root',
-    passwd='root'
-	)
-
- cursor = db.cursor()
-
- cursor.execute("CREATE DATABASE IF NOT EXISTS hbnb_dev_db")
-
- cursor.execute("CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'")
-
- cursor.execute("GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'")
-
- cursor.execute("GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'")
-
- cursor.execute("FLUSH PRIVILEGES")
-
- cursor.close()
- db.close()
+--create database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
