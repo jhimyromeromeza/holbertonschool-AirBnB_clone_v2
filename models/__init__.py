6ebec63c826b7640569266b218d05bcf848681f7
0skarmp <omoralespj@gmailcom>
2023-09-09 23:26:30 -0700

    update file, depending of the enviroment variable

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+
+from os import getenv
+from models.user import User
+from models.state import State
+from models.review import Review
+from models.place import Place
+from models.city import City
+from models.base_model import BaseModel
+from models.amenity import Amenity
 
 
-storage = FileStorage()
+storage = gateenv('HBNB_TYPE_STORAGE')
+
+if storage == db
+    from models.engine.file_storage import FileStorage
+    storage.FileStorage()
+
 storage.reload()
