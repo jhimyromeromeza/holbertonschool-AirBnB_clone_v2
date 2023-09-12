01dc796bd577ff373c974685c5b13c2a0386e6cb
0skarmp <omoralespj@gmail.com>
2023-09-12 10:21:11 -0700

    update, relationship

--- models/user.py
+++ models/user.py
@@ -3,14 +3,24 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
+from sqlalchemy.ext.declarative import declarative_base
 
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class User(BaseModel, Base):
+        """This class defines a user by various attributes"""
+        __tablename__ = "users"
 
-class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+        places = relationship ("Place", backref="user", cascade="all, delete")
 
+else:
+    class User(BaseModel):
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
