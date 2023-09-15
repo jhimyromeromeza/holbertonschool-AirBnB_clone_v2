62d4d9bb7004460243d36d3a92cd352cf05a1965
jhimyromeromeza <romeromezajhimy0>
2023-09-14 19:49:10 -0500

    document scrip of python

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""
+this is a simple flask module
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """
+    This function handles the root path of the application and returns the message 'Hello HBNB!'. 
+    """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
