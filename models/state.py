94904fb4128bb42a4e54c85aa7b59e762bacb022
jhimyromeromeza <romeromezajhimy0>
2023-09-17 17:43:18 -0500

    add list cities

--- models/state.py
+++ models/state.py
@@ -2,25 +2,29 @@
 """ City Module for HBNB project """
 from os import getenv
 import models
-from models.city import City
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class State(BaseModel, Base):
-    """ State class """
-    if getenv("HBNB_TYPE_STORAGE") == "db":
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    class State(BaseModel, Base):
+        """ State class """
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state", cascade="all, delete")
-    else:
+else:
+    class State(BaseModel):
         name = ""
-        cities = []
         
         @property
         def cities(self):
             """get a list of all related city instances
             with state_id = to the current state id
             """
-            return [city for city in models.storage.all(City).values() if city.state_id == self.id]
+            from models.city import City
+            list_cities = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
