e4925b8142160049746367b2f44805167e91251e
jhimyromeromeza <romeromezajhimy0>
2023-09-17 17:23:13 -0500

    add module City

--- models/state.py
+++ models/state.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from os import getenv
 import models
+from models.city import City
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
