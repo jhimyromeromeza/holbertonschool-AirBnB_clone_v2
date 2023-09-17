bd41e5827aea0e8bc817c610c2b6275b2aa1e971
jhimyromeromeza <romeromezajhimy0>
2023-09-17 17:49:53 -0500

    correct function

--- models/state.py
+++ models/state.py
@@ -18,9 +18,9 @@ class State(BaseModel, Base):
         name = ""
         cities = []
         
-        @property
-        def cities(self):
-            """get a list of all related city instances
-            with state_id = to the current state id
-            """
-            return [city for city in models.storage.all(City).values() if city.state_id == self.id]
+    @property
+    def cities(self):
+        """get a list of all related city instances
+        with state_id = to the current state id
+        """
+        return [city for city in models.storage.all(City).values() if city.state_id == self.id]
