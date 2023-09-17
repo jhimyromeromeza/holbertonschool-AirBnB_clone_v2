eaa2fba1d57766aecb1bb8b21dd8c03eb2048830
jhimyromeromeza <romeromezajhimy0>
2023-09-17 17:14:28 -0500

    changer state

--- models/state.py
+++ models/state.py
@@ -7,19 +7,20 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-if getenv("HBNB_TYPE_STORAGE") =="db":
-    class State(BaseModel, Base):
-        """class state"""
-        __tablename__ = "states"
-
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete")
-
-else:
-    name = ""
-    cities = []
-
-    @property
-    def cities(self):
-        """Obtiene todas las ciudades de un estado"""
-        return [city for city in models.storage.all(City).values() if city.state_id == self.id]
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+    
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """get a list of all related city instances
+            with state_id = to the current state id
+            """
+            cities_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
